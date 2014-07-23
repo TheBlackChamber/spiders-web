da4c90e5ced338b7abe90c15bfcf8e7d60ea46e2
Seamus <sminogue@yahoo.com>
2014-07-22 21:06:43 -0400

    added shortcut to take bug from bug list

---
+++ rest/takebug.php
@@ -0,0 +1,39 @@
+<?PHP
+require_once('dbsupport.php');
+try{
+	$bugid = $_REQUEST['bugid'];
+	
+	if (!isset($_COOKIE["auth"])){
+		http_response_code(401);
+	}else{
+
+		//Get cookie
+		$auth_cookie = json_decode($_COOKIE['auth']);
+
+		//Find user and compare to auth token from auth cookie
+		$get_user_query = $mysqli->prepare("SELECT id FROM user WHERE email = ? and auth_token = ?");
+		$get_user_query->bind_param('ss', $auth_cookie->{'email'},$auth_cookie->{'authtoken'});
+		$get_user_query->execute();
+		$get_user_query->bind_result($returned_user_id);
+
+		if($get_user_query->fetch()){
+			$get_user_query->free_result();
+			
+			$update_bug_query = $mysqli->prepare("UPDATE bug set assigned = ? where id = ?");
+			
+			$update_bug_query->bind_param('ii', $returned_user_id,$bugid);
+			$update_bug_query->execute();
+			$update_bug_query->close();
+
+			echo json_encode($bugid);
+
+		}else{
+			http_response_code(403);
+		}
+		$get_user_query->close();
+	}
+} catch (Exception $e) {
+	error_log('Unexpected Exception occurred: ' . $e);
+	http_response_code(500);
+}
+?>
